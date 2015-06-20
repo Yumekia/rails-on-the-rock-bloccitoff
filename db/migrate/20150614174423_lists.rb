ff7165278a41228bf28c93426784305a94f76741
yumekia <yumekiaw@yahoo.com>
2015-06-20 14:45:34 +0000

    wip seed data

--- db/migrate/20150614174423_lists.rb
+++ db/migrate/20150614174423_lists.rb
@@ -1,4 +1,11 @@
 class Lists < ActiveRecord::Migration
   def change
+    create_table :lists do |t|
+      t.string :title
+      t.references :user
+      
+      t.timestamps null: false
+  end
+  add_foreign_key :lists, :users
   end
 end
--- db/migrate/20150614174449_tasks.rb
+++ db/migrate/20150614174449_tasks.rb
@@ -1,4 +1,13 @@
 class Tasks < ActiveRecord::Migration
   def change
+    create_table :tasks do |t|
+      t.text :description
+      t.references :list, index: true
+      t.references :user, index: true
+      
+      t.timestamps null false
+  end
+    add_foreign_key :tasks, :list
+    add_foreign_key :tasks, :users
   end
 end
